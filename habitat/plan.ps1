5562cbd41b78f3c53be88a0a76db031d1858c73c
Nick Rycar <nickrycar@Nicks-MacBook-Pro.local>
2020-02-18 12:26:40 -0800

    Change origin for my fork.

--- habitat/plan.ps1
+++ habitat/plan.ps1
@@ -1,5 +1,5 @@
 $pkg_name="contosouniversity"
-$pkg_origin="mwrock"
+$pkg_origin="nrycar"
 $pkg_version="0.1.0"
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license=@("Apache-2.0")
